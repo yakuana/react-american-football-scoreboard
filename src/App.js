d3a43b744c9601a138af3643de2dd0b53bd87c4b
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-07-15 15:10:24 -0500

    Stretch WIP: ScoreBoard component created and functioning.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, {useState} from "react";
 import "./App.css";
+import ScoreBoard from "./ScoreBoard"; 
 import BottomRow from "./BottomRow";
+
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
@@ -11,23 +13,7 @@ function App() {
 
   return (
     <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{homeScore}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
+      <ScoreBoard home = {homeScore} away ={awayScore}/>
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
@@ -44,3 +30,25 @@ function App() {
 }
 
 export default App;
+
+
+/**
+ * <section className="scoreboard">
+        <div className="topRow">
+          <div className="home">
+            <h2 className="home__name">Lions</h2>
+
+            // {TODO STEP 3 - We need to change the hardcoded values in these 
+            // divs to accept dynamic values from our state.}
+
+            <div className="home__score">{homeScore}</div>
+          </div>
+          <div className="timer">00:03</div>
+          <div className="away">
+            <h2 className="away__name">Tigers</h2>
+            <div className="away__score">{awayScore}</div>
+          </div>
+        </div>
+        <BottomRow />
+      </section>
+ */
